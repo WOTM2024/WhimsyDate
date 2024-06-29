2f21e3e98502fdc55bfe4a74eee0f510b9caa155
John <jrnoden@googlemail.com>
2024-06-29 06:37:48 +0100

    Updates testing

--- server/__tests__/foods.test.js
+++ server/__tests__/foods.test.js
@@ -1,17 +1,5 @@
 const request = require("supertest");
 const app = require("../app");
-const db = require("../db/connection");
-const seed = require("../db/seeds/seed");
-const data = require("../db/data/test-data");
-
-afterAll(() => {
-    return db.end();
-});
-
-beforeEach(() => {
-    console.log("seeding!");
-    return seed(data);
-});
 
 describe("GET: /foods", () => {
     test("200: responds with an array of all foods", () => {
@@ -35,7 +23,7 @@ describe("GET: /foods", () => {
 
     test("404: ERROR - responds with an error when endpoint does not exist", () => {
         return request(app)
-            .get("/nonexistent-endpoint")
+            .get("/not-an-endpoint")
             .expect(404)
             .then(({ body }) => {
                 expect(body.msg).toBe("Not Found");
