77efad10a545e9b6a326d5bccd7bc99887797624
ruchikaroy <contactruchikaroy@gmail.com>
2024-07-02 10:31:41 +0100

    This commit will implement moongose db database creation

--- db/data/test-data/index.js
+++
@@ -1,6 +0,0 @@
-exports.userData = require("./users.js");
-exports.activitiesData = require("./activities.js");
-exports.foodData = require("./foods.js");
-exports.filmsData = require("./films.js");
-exports.tv_showsData = require("./tv_shows.js");
-exports.couplesData = require("./couples.js");
--- db/setup.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS whimsy_dates_test;
--- DROP DATABASE IF EXISTS whimsy_dates;
-CREATE DATABASE whimsy_dates_test;
--- CREATE DATABASE whimsy_dates;
