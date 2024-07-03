5bdcdf66b145381eb871be58e43357274773b1a1
John <jrnoden@googlemail.com>
2024-07-03 09:26:00 +0100

    Adds initial testing for foods

---
+++ server/__tests__/foods.test.js
@@ -0,0 +1,29 @@
+describe("GET: /foods", () => {
+    test("200: responds with an array of all foods", () => {
+        return request(app)
+            .get("/foods")
+            .expect(200)
+            .then(({ body }) => {
+                expect(body.success).toBe(true);
+                expect(body.data).toBeInstanceOf(Array);
+                body.data.forEach((food) => {
+                    expect(food).toMatchObject({
+                        food: expect.any(String),
+                        vegetarian: expect.any(Boolean),
+                        vegan: expect.any(Boolean),
+                        meat: expect.any(Boolean),
+                        allergies: expect.any(Boolean)
+                    });
+                });
+            });
+    });
+
+    test("404: ERROR - responds with an error when endpoint does not exist", () => {
+        return request(app)
+            .get("/notAnEndpoint")
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.msg).toBe("");
+            });
+    });
+});
