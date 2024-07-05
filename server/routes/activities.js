19976d4aaa0db87614f29068deb968c03c3d3f51
ruchikaroy <contactruchikaroy@gmail.com>
2024-07-05 11:28:29 +0100

    This commit adds and updates endpoints json file

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getActivities, postActivities } = require("../controllers/activities-controller");
+const {
+  getActivities,
+  postActivities,
+} = require("../controllers/activities-controller");
 
 activities_router = express.Router();
 
 activities_router.get("/", getActivities);
-activities_router.post("/add", postActivities);
+activities_router.post("/", postActivities);
 
 module.exports = activities_router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const { postUser, getUsers, deleteUser, getUserCategories } = require("../controllers/users-controller");
+const {
+  postUser,
+  getUsers,
+  deleteUser,
+  getUserCategories,
+} = require("../controllers/users-controller");
 
 const router = express.Router();
 
 router.get("/", getUsers);
-router.post("/add", postUser);
+router.post("/", postUser);
 router.delete("/delete", deleteUser);
 router.get("/categories", getUserCategories);
-
 
 module.exports = router;
