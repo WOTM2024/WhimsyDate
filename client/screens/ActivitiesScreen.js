ace7d7757b55b9bc84bb7258a202708fbd56140c
Tian <66271788+TianYu-00@users.noreply.github.com>
2024-07-10 16:22:24 +0100

    prettier reformat

--- client/screens/ActivitiesScreen.js
+++ client/screens/ActivitiesScreen.js
@@ -1,22 +1,17 @@
 import * as React from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  ScrollView,
-  SafeAreaView,
-  Modal,
-} from "react-native";
+import { View, Text, TouchableOpacity, TextInput, ScrollView, SafeAreaView, Modal } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import {
-  MagnifyingGlassIcon,
-  MinusIcon,
-  PlusCircleIcon,
-} from "react-native-heroicons/outline";
+import { MagnifyingGlassIcon, MinusIcon, PlusCircleIcon } from "react-native-heroicons/outline";
 import { LinearGradient } from "expo-linear-gradient";
 import { auth } from "../firebase";
-import { fetchActivities, fetchFoods, fetchMovies, fetchTvShows, fetchUserByUID, patchUserEntriesByEntryId } from "../api";
+import {
+  fetchActivities,
+  fetchFoods,
+  fetchMovies,
+  fetchTvShows,
+  fetchUserByUID,
+  patchUserEntriesByEntryId,
+} from "../api";
 import AddActivityForm from "../components/AddActivityForm";
 
 export default function ActivitiesScreen() {
@@ -41,28 +36,22 @@ export default function ActivitiesScreen() {
           const userResponse = await fetchUserByUID(userId);
           const userData = userResponse.data.data;
 
-          const activities = await fetchActivities()
-          const foods = await fetchFoods()
-          const tvShows = await fetchTvShows()
-          const movies = await fetchMovies()
+          const activities = await fetchActivities();
+          const foods = await fetchFoods();
+          const tvShows = await fetchTvShows();
+          const movies = await fetchMovies();
 
-          const filteredActivities = activities.filter(activity => userData.user_activities.includes(activity._id));
-          const filteredFoods = foods.filter(food => userData.user_food_choices.includes(food._id));
-          const filteredTvShows = tvShows.filter(tvShow => userData.user_tv_shows.includes(tvShow._id));
-          const filteredMovies = movies.filter(movie => userData.user_films.includes(movie._id));
-
+          const filteredActivities = activities.filter((activity) => userData.user_activities.includes(activity._id));
+          const filteredFoods = foods.filter((food) => userData.user_food_choices.includes(food._id));
+          const filteredTvShows = tvShows.filter((tvShow) => userData.user_tv_shows.includes(tvShow._id));
+          const filteredMovies = movies.filter((movie) => userData.user_films.includes(movie._id));
 
           setUserActivities(filteredActivities);
           setUserFoods(filteredFoods);
           setUserTvShows(filteredTvShows);
           setUserMovies(filteredMovies);
 
-          setFilteredData([
-            ...filteredActivities,
-            ...filteredFoods,
-            ...filteredTvShows,
-            ...filteredMovies,
-          ]);
+          setFilteredData([...filteredActivities, ...filteredFoods, ...filteredTvShows, ...filteredMovies]);
         }
       } catch (err) {
         console.error("Error fetching user data:", err);
@@ -79,9 +68,6 @@ export default function ActivitiesScreen() {
     try {
       const user = auth.currentUser;
       if (user) {
-
-
-
         switch (category) {
           case "user_activities":
             setUserActivities((prev) => {
@@ -115,14 +101,13 @@ export default function ActivitiesScreen() {
             console.error(`Unknown category: ${category}`);
             break;
         }
-        await patchUserEntriesByEntryId(user.uid, category, entryId)
-
+        await patchUserEntriesByEntryId(user.uid, category, entryId);
       }
     } catch (err) {
       console.error("Error removing item", err);
       fetchUserData();
     }
-  }
+  };
 
   const handleAddActivity = (newActivity) => {
     setUserActivities((prev) => [...prev, newActivity]);
@@ -130,33 +115,20 @@ export default function ActivitiesScreen() {
   };
 
   React.useEffect(() => {
-    const combinedData = [
-      ...userActivities,
-      ...userFoods,
-      ...userTvShows,
-      ...userMovies,
-    ];
+    const combinedData = [...userActivities, ...userFoods, ...userTvShows, ...userMovies];
 
     if (searchForQuery === "") {
       setFilteredData(combinedData);
     } else {
       const query = searchForQuery.toLowerCase();
       setFilteredData(
-        [...userActivities, ...userFoods, ...userTvShows, ...userMovies].filter(
-          (item) => {
-            const name =
-              item.activity_name ||
-              item.food_name ||
-              item.show ||
-              item.film ||
-              "";
-            return name.toLowerCase().includes(query);
-          }
-        )
+        [...userActivities, ...userFoods, ...userTvShows, ...userMovies].filter((item) => {
+          const name = item.activity_name || item.food_name || item.show || item.film || "";
+          return name.toLowerCase().includes(query);
+        })
       );
     }
   }, [searchForQuery, userActivities, userFoods, userTvShows, userMovies]);
-
 
   // function onPressHandle_searchActivities() {
   //   console.log("Pressed Search");
@@ -203,7 +175,7 @@ export default function ActivitiesScreen() {
               size={25}
               color="#1E1E1E"
               className="ml-2"
-            // onPress={onPressHandle_searchActivities}
+              // onPress={onPressHandle_searchActivities}
             />
           </View>
         </View>
@@ -211,10 +183,7 @@ export default function ActivitiesScreen() {
           <Text className="text-xl font-bold">Activities</Text>
           {userActivities.map((activity, index) => {
             return (
-              <View
-                key={index}
-                className="flex-row items-center justify-between border p-3 m-2 rounded-xl"
-              >
+              <View key={index} className="flex-row items-center justify-between border p-3 m-2 rounded-xl">
                 <Text className="flex-1 font-bold text-light_text text-lg">
                   {activity.activity_name || "Unknown Activity"}
                 </Text>
@@ -230,13 +199,8 @@ export default function ActivitiesScreen() {
           <Text className="text-xl font-bold">Foods</Text>
           {userFoods.map((food, index) => {
             return (
-              <View
-                key={index}
-                className="flex-row items-center justify-between border p-3 m-2 rounded-xl"
-              >
-                <Text className="flex-1 font-bold text-light_text text-lg">
-                  {food.food_name || "Unknown Food"}
-                </Text>
+              <View key={index} className="flex-row items-center justify-between border p-3 m-2 rounded-xl">
+                <Text className="flex-1 font-bold text-light_text text-lg">{food.food_name || "Unknown Food"}</Text>
                 <MinusIcon
                   size={25}
                   color="#1E1E1E"
@@ -249,13 +213,8 @@ export default function ActivitiesScreen() {
           <Text className="text-xl font-bold">TV Shows</Text>
           {userTvShows.map((tvShow, index) => {
             return (
-              <View
-                key={index}
-                className="flex-row items-center justify-between border p-3 m-2 rounded-xl"
-              >
-                <Text className="flex-1 font-bold text-light_text text-lg">
-                  {tvShow.show || "Unknown Show"}
-                </Text>
+              <View key={index} className="flex-row items-center justify-between border p-3 m-2 rounded-xl">
+                <Text className="flex-1 font-bold text-light_text text-lg">{tvShow.show || "Unknown Show"}</Text>
                 <MinusIcon
                   size={25}
                   color="#1E1E1E"
@@ -268,13 +227,8 @@ export default function ActivitiesScreen() {
           <Text className="text-xl font-bold">Movies</Text>
           {userMovies.map((movie, index) => {
             return (
-              <View
-                key={index}
-                className="flex-row items-center justify-between border p-3 m-2 rounded-xl"
-              >
-                <Text className="flex-1 font-bold text-light_text text-lg">
-                  {movie.film || "Unknown Movie"}
-                </Text>
+              <View key={index} className="flex-row items-center justify-between border p-3 m-2 rounded-xl">
+                <Text className="flex-1 font-bold text-light_text text-lg">{movie.film || "Unknown Movie"}</Text>
                 <MinusIcon
                   size={25}
                   color="#1E1E1E"
@@ -286,11 +240,7 @@ export default function ActivitiesScreen() {
           })}
         </ScrollView>
         <View>
-          <PlusCircleIcon
-            size={75}
-            color="#1E1E1E"
-          onPress={() => setIsModalVisible(true)}
-          />
+          <PlusCircleIcon size={75} color="#1E1E1E" onPress={() => setIsModalVisible(true)} />
         </View>
       </SafeAreaView>
       <Modal
@@ -299,14 +249,8 @@ export default function ActivitiesScreen() {
         visible={isModalVisible}
         onRequestClose={() => setIsModalVisible(false)}
       >
-        <AddActivityForm
-          onClose={() => setIsModalVisible(false)}
-          onAddActivity={handleAddActivity}
-        />
+        <AddActivityForm onClose={() => setIsModalVisible(false)} onAddActivity={handleAddActivity} />
       </Modal>
     </LinearGradient>
   );
 }
-
-
-
